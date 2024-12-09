0ae3e1c6a5d0bbf30e2f03af2393fb865193a2ae
czenturion <czenturijon@email.com>
2024-12-09 23:36:07 +0300

    feat: api & hndlers

--- api/requests.js
+++ api/requests.js
@@ -1,48 +1,65 @@
 import { instance } from "./api.js";
+import { showError } from "../scripts/handlers.js";
 
 export const API = {
-    async getProducts(limit = 6) {
-        try {
-            const res = await instance.get(`/products?limit=${limit}`)
-            return res.data
-        } catch (error) {
-            console.error('Error while getting products: ', error)
-        }
-    },
+  async getProducts(limit = 6) {
+    try {
+      const res = await instance.get(`/products?limit=${limit}`);
 
-    async addNewProduct(product) {
-        try {
-            const res = await instance.post('/products', {
-                title: product.name,
-                price: product.price,
-                description: product.description,
-                image: 'https://i.pravatar.cc',
-                category: product.category
-            })
-
-            return res.data
-        } catch (error) {
-            console.error(error)
-        }
-    },
-
-    async getAllCategories() {
-        try {
-            const res = await instance.get('/products/categories')
-
-            return res.data
-        } catch (error) {
-            console.error(error)
-        }
-    },
-
-    async getProductsByCategory(cat) {
-        try {
-            const res = await instance.get(`/products/category/${cat}`)
-
-            return res.data
-        } catch (error) {
-            console.error(error)
-        }
+      return res.data;
+    } catch (error) {
+      console.error("Error while getting products: ", error);
+      showError("Get all products error, try again later.");
     }
-}
+  },
+
+  async addNewProduct(product) {
+    try {
+      const res = await instance.post("/products", {
+        title: product.title,
+        price: product.price,
+        description: product.description,
+        image: "https://i.pravatar.cc",
+        category: product.category,
+      });
+
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      showError("Add new product error, try again later.");
+    }
+  },
+
+  async getAllCategories() {
+    try {
+      const res = await instance.get("/products/categories");
+
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      showError("Get all catergories error, try again later.");
+    }
+  },
+
+  async getProductsByCategory(cat) {
+    try {
+      const res = await instance.get(`/products/category/${cat}`);
+
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      showError("Get product by category error, try again later.");
+    }
+  },
+
+  async deleteProduct(id) {
+    try {
+      const res = await instance.delete(`/products/${id}`);
+
+      return res.data;
+    } catch (error) {
+      console.error(error);
+      showError("Delete product error, try again later.");
+    }
+  },
+};
---
+++ scripts/handlers.js
@@ -0,0 +1,51 @@
+export const errorMsg = () => {
+  const errorDiv = document.createElement("div")
+  errorDiv.classList.add("error-result")
+  errorDiv.innerHTML = "Products load error, Please refresh page.";
+  productList.appendChild(errorDiv);
+  return;
+};
+
+export const createProductCard = (product, deleteProduct) => {
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
+  productCard.setAttribute("product-id", product.id);
+
+  productCard.innerHTML = `
+            <img src=${product.image} alt='Product image'/>
+            <h3>${product.title}</h3>
+            <p>${product.description}</p>
+            <p><strong>${product.price} $</strong></p>
+            <button class="delete-btn">X</button>
+        `;
+
+  productCard.querySelector(".delete-btn").addEventListener("click", (e) => {
+    e.stopPropagation();
+    deleteProduct(product.id);
+  });
+
+  return productCard;
+};
+
+export const showError = (message) => {
+  const errorDiv = document.createElement("div");
+  errorDiv.textContent = message;
+  errorDiv.classList.add("error-message", "lato-regular");
+
+  const mainContainer = document.querySelector("main");
+  mainContainer.prepend(errorDiv);
+
+  setTimeout(() => {
+    errorDiv.remove();
+  }, 5000);
+};
+
+export const showNotification = (message, duration = 3000) => {
+  const notification = document.querySelector("#notification");
+  notification.textContent = message;
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, duration);
+};
